4ced0dc2b722c3b873702d2f46288b022405665d
linas19 <34280196+linas19@users.noreply.github.com>
2018-11-04 14:09:05 +0200

    Update CPU_tracker.py

--- CPU_tracker.py
+++ CPU_tracker.py
@@ -1,58 +1,11 @@
 import smtplib
 import psutil
 import time
-print('─────────────────█─────────────────')
-time.sleep(0.1)
-print('────────────────███────────────────')
-time.sleep(0.1)
-print('───────────────█████───────────────')
-time.sleep(0.1)
-print('───────────────██─██───────────────')
-time.sleep(0.1)
-print('──────────────███─███──────────────')
-time.sleep(0.1)
-print('──────────────██───██──────────────')
-print('─────────────███───███─────────────')
-print('────────────███─────███────────────')
-time.sleep(0.1)
-print('────────────███─────███────────────')
-print('───────────███──LJ───███───────────')
-print('───────────███───────███───────────')
-print('──────────███─────────███──────────')
-time.sleep(0.1)
-print('──────────███─────────███──────────')
-print('──────────███─────────███──────────')
-print('─────────███───────────███─────────')
-print('─────────███───────────███─────────')
-time.sleep(0.1)
-print('────────███─────────────███────────')
-print('───────████─────────────████───────')
-print('▄───▄▄████───────────────████▄▄───▄')
-print('▀█████████───█████████───█████████▀')
-print('──███████──█████████████──███████──')
-print('───██████─███████████████─██████───')
-time.sleep(0.1)
-print('───█████──███████████████──█████───')
-print('───█████──███──█████──███──█████───')
-print('──█████────██───███───██────█████──')
-print('▄██████───██████▀─▀██████───██████▄')
-print('▀──▀███────█████▄█▄█████────███▀──▀')
-time.sleep(0.1)
-print('────▀███▄────██▀█▀█▀██────▄███▀────')
-print('▀▄─────▀██▄▄──█▄█▄█▄█──▄▄██▀─────▄▀')
-print('──▀▄─────▀▀██▄▄──▄──▄▄██▀▀─────▄▀──')
-print('────█▄──────────▄█▄──────────▄█────')
-print('─────▀██▄──────▄███▄──────▄▄█▀─────')
-print('───────▀███▄▄▄███████▄▄▄███▀───────')
-print('──────────▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀──────────')
 print("Welcome!!! This program automatically tracks the usage of CPU and if percentage \nfalls bellow asigned value sends e-mail notifying about it")
-
 print("Enter CPU percentage to track: ")
 target_percentage = input()
 print("Enter your e-mail: ")
-
 target_mail = input()
-
 # Check CPU status
 while True:
 	x = psutil.cpu_percent(interval=3)
@@ -62,10 +15,8 @@ while True:
 		break
 	elif x > int(target_percentage):
 		print("Above given value")
-
 # Start SMTP server and send mail
 mail = smtplib.SMTP('smtp.gmail.com', 587)
-
 mail.ehlo()
 mail.starttls()
 mail.login('', '')
